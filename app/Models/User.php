3230831395115f87721478174210375e996b84d9
Marcus Vinicius <marcus_viniciusgama@hotmail.com>
2022-08-06 13:54:40 -0300

    First Commit

---
+++ app/Models/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    use HasFactory;
+    protected $table = 'users';
+    protected $fillable = ['name', 'email', 'cpf', 'password', 'role'];
+    protected $hidden = ['password'];
+
+    //relacionamento
+    public function address(){
+        return $this->hasOne(Address::class);
+    }
+    public function phones(){
+        return $this->hasMany(Phone::class);
+    }
+    
+    //mutators
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+}
---
+++ database/migrations/2022_08_06_134111_create_phones_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function PHPUnit\Framework\once;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
